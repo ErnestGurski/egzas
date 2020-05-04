5c64c2800ce2dd660e523f1247a56d9e150badf6
ErnestGurski <64586209+ErnestGurski@users.noreply.github.com>
2020-05-04 10:37:16 +0300

    Create suo.cs

---
+++ suo.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace Ernest
+{
+    class Suo : Zinduoliai
+    {
+        public string gyvunas { get; set; }
+        public int Line { get; set; }
+
+        private enum gyvunass
+        {
+            meska,
+            leopardas,
+            gepardas,
+            zebra
+        }
+
+        public Suo(int svoris, int ugis, string pav) : base(svoris, ugis, pav)
+        {
+            this.gyvunas = gyvunas;
+        }
+
+        public static void GetAvailablegyvunass()
+        {
+            var gyvunass = Enum.GetValues(typeof(gyvunass));
+            Console.WriteLine("Available gyvunass:");
+            foreach (var gyvunas in gyvunass)
+            {
+                Console.Write(gyvunas.ToString() + " ");
+            }
+        }
+
+        public override string Getgyvunas()
+        {
+            return this.gyvunas;
+        }
+       
+
+        public void PrintAbout()
+        {
+            Console.WriteLine("Gyvunas:", this.Line, Getgyvunas());
+            PrintPerfumeBase();
+        }
+
+        ~Suo()
+        {
+            Console.WriteLine("Išlavyti");
+        }
+
+    }
+}
